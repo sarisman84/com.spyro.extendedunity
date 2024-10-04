9091c018866d1683f741071795ad496077866325
Spyridon Passas <spyridonpassas0325@gmail.com>
2024-10-04 20:34:02 +0200

    Fixed the issue where the engine would stall due to a recursive draw. Massively simplified the logic to draw sub editors. Added color variation per sub editor.

--- Editor/RecursiveScriptableObject/RecursiveScriptableObjectPropertyDrawer.cs
+++ Editor/RecursiveScriptableObject/RecursiveScriptableObjectPropertyDrawer.cs
@@ -1,29 +1,47 @@
 using Spyro.EditorExtensions;
 using Spyro.UI;
+using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
 using UnityEditor;
 using UnityEditor.UIElements;
 using UnityEngine;
 using UnityEngine.UIElements;
+using Random = UnityEngine.Random;
 
 [CustomPropertyDrawer(typeof(RecursiveAttribute))]
 public class RecursiveScriptableObjectPropertyDrawer : PropertyDrawer
 {
-    private static Dictionary<int, bool> _activeStates = new Dictionary<int, bool>();
-    private static Dictionary<int, int> _recursiveCount = new Dictionary<int, int>();
+    struct SubEditorArgs
+    {
+        public ScrollView currentSubEditor;
+        public SerializedProperty currentProperty;
+        public ToolbarToggle currentEditButton;
+        public SerializedObject currentOwner;
+    }
+
+    struct SubEditorInfo
+    {
+        public bool activeState;
+        public Color backgroundColor;
+    }
+
+    private static Dictionary<int, SubEditorInfo> _editorInfo = new Dictionary<int, SubEditorInfo>();
     private ToolbarToggle _subEditorToggle;
     private SerializedProperty _scriptField;
     private SubEditorArgs _subEditorArgs;
     private int _maxRenderingCount = 10;
 
-    private static VisualElement _root;
+    private const int INVALID_KEY = -1;
 
-    struct SubEditorArgs
+    public Color GenerateRandomColor()
     {
-        public ScrollView currentSubEditor;
-        public UnityEngine.Object currentObjectInstance;
-        public ToolbarToggle currentEditButton;
+        Color color = Random.ColorHSV(1.0f, 1.0f, 1.0f, 1.0f, 0.15f, 0.85f, 1.0f, 1.0f);
+
+        return color;
     }
+
     public override VisualElement CreatePropertyGUI(SerializedProperty property)
     {
         // if (_root == null)
@@ -35,14 +53,7 @@ public class RecursiveScriptableObjectPropertyDrawer : PropertyDrawer
 
     private VisualElement InitializeProperty(SerializedProperty property)
     {
-        Debug.Log("Recursive Scripting init");
-
         var hashCode = !property.objectReferenceValue ? -1 : property.objectReferenceValue.GetInstanceID();
-        if (!_activeStates.ContainsKey(hashCode))
-        {
-            _activeStates.Add(hashCode, false);
-        }
-
         var root = new VisualElement();
 
         var uxmlAsset = UIToolkitUtility.GetUXML("URecursiveScriptableEditor");  /*AssetDatabase.LoadAssetAtPath<VisualTreeAsset>("Packages/com.spyro.extendedunity/UI/URecursiveScriptableEditor.uxml");*/
@@ -59,20 +70,39 @@ public class RecursiveScriptableObjectPropertyDrawer : PropertyDrawer
         _subEditorArgs = new SubEditorArgs
         {
             currentSubEditor = subEditor,
-            currentObjectInstance = property.objectReferenceValue,
-            currentEditButton = subEditorToggle
+            currentProperty = property,
+            currentEditButton = subEditorToggle,
+            currentOwner = property.serializedObject
         };
 
-        subEditorToggle.RegisterCallback<ChangeEvent<bool>, SubEditorArgs>((evt, args) => ViewSubEditor(evt, args, hashCode), _subEditorArgs);
+        subEditorToggle.RegisterCallback<ChangeEvent<bool>, SubEditorArgs>(ViewSubEditor, _subEditorArgs);
         subEditor.style.display = DisplayStyle.None;
         subEditor.verticalScroller.Adjust(0);
+
+        var key = GetKey(_subEditorArgs);
+
+        if (IsKeyValid(key))
+        {
+            var info = _editorInfo[key];
+            subEditor.style.backgroundColor = info.backgroundColor;
+        }
 
         _subEditorToggle = subEditorToggle;
         _scriptField = property;
 
-        ViewSubEditor(_subEditorArgs, hashCode);
+        ViewSubEditor(_subEditorArgs);
+
+        root.RegisterCallback<DetachFromPanelEvent>(EditorCleanup);
 
         return root;
+    }
+
+    private void EditorCleanup(DetachFromPanelEvent evt)
+    {
+        VisualElement root = evt.currentTarget as VisualElement;
+        root.ClearBindings();
+
+
     }
 
     private void OnPropertyUpdated(SerializedPropertyChangeEvent evt)
@@ -80,32 +110,93 @@ public class RecursiveScriptableObjectPropertyDrawer : PropertyDrawer
         _subEditorToggle.style.display = _scriptField.objectReferenceValue != null ? DisplayStyle.Flex : DisplayStyle.None;
     }
 
-    private void ViewSubEditor(SubEditorArgs args, int hashCode)
+    private PropertyField DrawProperty(SerializedProperty property, SerializedObject owner)
+    {
+        var propertyField = new PropertyField(property);
+        propertyField.Bind(owner);
+        propertyField.style.width = new Length(95.0f, LengthUnit.Percent);
+        return propertyField;
+    }
+
+    private void ViewSubEditor(SubEditorArgs args)
     {
         args.currentSubEditor.Clear();
-        if (!_activeStates[hashCode] || args.currentObjectInstance == null)
+        if (!CanViewSubEditor(args))
         {
             args.currentSubEditor.verticalScroller.Adjust(0);
             args.currentSubEditor.style.display = DisplayStyle.None;
-            args.currentSubEditor.style.display = args.currentObjectInstance != null ? DisplayStyle.Flex : DisplayStyle.None;
+            args.currentSubEditor.style.display = args.currentProperty.objectReferenceValue != null ? DisplayStyle.Flex : DisplayStyle.None;
             return;
         }
-        args.currentSubEditor.style.display = DisplayStyle.Flex;
-        //Traverse through the property's children if it contains an entry and add them to the sub editor object.
-        var serializedObject = new SerializedObject(args.currentObjectInstance);
-        foreach (var child in serializedObject.FindVisibleChildProperties())
+
+        if (!args.currentProperty.objectReferenceValue)
         {
-            if (child.name == "m_Script" || child.propertyPath == "m_Script")
-                continue;
-            var propertyField = new PropertyField(child);
-            propertyField.Bind(serializedObject);
-            propertyField.style.width = new Length(99.6f, LengthUnit.Percent);
-            args.currentSubEditor.Add(propertyField);
+            return;
         }
+
+        var serializedObject = new SerializedObject(args.currentProperty.objectReferenceValue);
+        args.currentSubEditor.style.display = DisplayStyle.Flex;
+
+
+        var element = new InspectorElement();
+        element.Bind(serializedObject);
+        args.currentSubEditor.Add(element);
     }
-    private void ViewSubEditor(ChangeEvent<bool> env, SubEditorArgs args, int hashCode)
+
+    private int GetKey(SubEditorArgs args)
     {
-        _activeStates[hashCode] = !_activeStates[hashCode];
-        ViewSubEditor(args, hashCode);
+        if (!args.currentProperty.objectReferenceValue)
+        {
+            return INVALID_KEY;
+        }
+
+        var instanceID = args.currentProperty.objectReferenceValue.GetInstanceID();
+        var hashCode = args.currentProperty.name.GetHashCode();
+
+        var key = instanceID + hashCode;
+        return key;
+    }
+
+    private bool IsKeyValid(int key)
+    {
+        if (key == INVALID_KEY)
+        {
+            return false;
+        }
+
+        return _editorInfo.ContainsKey(key);
+    }
+
+    private bool CanViewSubEditor(SubEditorArgs args)
+    {
+        if (!args.currentProperty.objectReferenceValue)
+        {
+            return false;
+        }
+
+
+        var key = GetKey(args);
+        _editorInfo.TryAdd(key, new SubEditorInfo
+        {
+            activeState = false,
+            backgroundColor = GenerateRandomColor()
+        });
+
+        return _editorInfo[key].activeState;
+
+    }
+
+    private void ViewSubEditor(ChangeEvent<bool> env, SubEditorArgs args)
+    {
+        var key = GetKey(args);
+        if (_editorInfo.ContainsKey(key))
+        {
+            var temp = _editorInfo[key];
+            temp.activeState = !_editorInfo[key].activeState;
+            _editorInfo[key] = temp;
+        }
+
+        ViewSubEditor(args);
+        //Debug.Log($"Editor {hashCode} is {(_activeStates[hashCode] ? "Visible" : "Not Visible")}");
     }
 }
